cb1063af99b7d637ae5970444a7a33a154dcd72a
Christian Y. Castillo Covarrubias <cyccov@gmail.com>
2021-11-13 15:17:00 -0600

    -
    -

--- training/t_flutter/lib/main.dart
+++ training/t_flutter/lib/main.dart
@@ -15,6 +15,52 @@ class MyApp extends StatefulWidget {
 }
 
 class _State extends State<MyApp> {
+
+  String _value = 'Hello World!!';
+  int _integerValue = 0;
+  String _nombre = '';
+  String _valueInput = '';
+
+
+    void _onClick(){
+      setState((){
+        _value = 'my name is christian';
+      });
+    }
+
+    void _add(){
+      setState((){
+        _integerValue++;
+      });
+    }
+
+    void _substract(){
+      setState((){
+        _integerValue--;
+      });
+    }
+
+    void _onPressed(String value){
+      setState((){
+        _value = value;
+      });
+    }
+
+    void _onPressedButton(){
+      setState((){
+        _value = DateTime.now().toString();
+      });
+      }
+
+    void _onPressedGetName(){
+      setState((){
+        _nombre = 'Christian Castillo';
+      });
+    }
+
+    void _onChangeInput() => setState(() => _value = 'Change: $_valueInput');
+    void _onSubmit() => setState(() => _value = 'Input: $_valueInput');
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -25,8 +71,18 @@ class _State extends State<MyApp> {
           padding: const EdgeInsets.all(32.0),
           child: Center(
             child: Column(
-              children: const <Widget>[
-                Text('Hello World..')
+              children: <Widget>[
+                Text(_value),
+                Text('Valor: $_integerValue'),
+                Text('Nombre: $_nombre'),
+                ElevatedButton(onPressed:  () => _onPressed('BLA'), child: const Text('Click me!!!!')),
+                TextButton(onPressed: () => _onPressedButton(), child: const Text('Click me!')),
+                IconButton(onPressed: _add, icon: const Icon(Icons.add)),
+                IconButton(onPressed: _substract, icon: const Icon(Icons.remove)),
+                TextButton(onPressed: _onPressedGetName, child: Text('OBtener nombres----')),
+                const Text('--------INPUT WIDGETSSS---------'),
+                Text('$_valueInput'),
+                TextField(),
               ]
             )
           )
